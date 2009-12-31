808cd0f8ac27a9f113efddba50a37837f364723e
Brian Rosner <brosner@gmail.com>
2009-12-31 15:11:06 -0700

    Revert "added GFK for group"
    This reverts commit 957e11ef62823a29472eeec4dade65ae01bbea70.

--- idios/models.py
+++ idios/models.py
@@ -5,18 +5,12 @@ from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
 
 
 
 class ProfileBase(models.Model):
     
     user = models.ForeignKey(User, unique=True, verbose_name=_("user"))
-    
-    group_content_type = models.ForeignKey(ContentType, null=True, blank=True)
-    group_object_id = models.PositiveIntegerField(null=True, blank=True)
-    group = generic.GenericForeignKey("group_content_type", "group_object_id")
     
     class Meta:
         verbose_name = _("profile")
