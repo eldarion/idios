519d53efeb815843fa33517adf4d737aa9ad9c92
Brian Rosner <brosner@gmail.com>
2010-01-01 15:43:52 -0700

    use get_profile_model in admin.py

--- idios/admin.py
+++ idios/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from idios.models import Profile
+from idios.utils import get_profile_model
 
 
 
-admin.site.register(Profile)
+admin.site.register(get_profile_model())
