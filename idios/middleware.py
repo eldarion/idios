5456a9c3a6c21a1969218994bcd77d2cccae07f7
Brian Rosner <brosner@gmail.com>
2010-05-13 04:16:55 -0600

    use request.path as the redirect (good ol' 4 AM programming)

--- idios/middleware.py
+++ idios/middleware.py
@@ -7,7 +7,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.views.decorators.csrf import csrf_protect
 
-from idios.utils import get_profile_model, get_default_redirect
+from idios.utils import get_profile_model
 
 
 class AdditionalInfoMiddleware(object):
@@ -58,7 +58,7 @@ def handle_additional_info(request):
                 for field, value in form.cleaned_data.iteritems():
                     setattr(profile, field, value)
                 profile.save()
-                return redirect(get_default_redirect(request))
+                return redirect(request.path)
         else:
             form = AdditionalInfoForm()
         ctx = {
--- idios/utils.py
+++ idios/utils.py
@@ -9,7 +9,6 @@ modified for Eldarion standards.
 """
 from django import forms
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.db.models import get_model
 
 from django.contrib.auth.models import SiteProfileNotAvailable
@@ -47,31 +46,3 @@ def get_profile_form():
             model = profile_mod
             exclude = ("user",) # user will be filled in by the view.
     return _ProfileForm
-
-
-def get_default_redirect(request, redirect_field_name="next",
-        login_redirect_urlname=None, session_key_value="redirect_to"):
-    """
-    Returns the URL to be used in login procedures by looking at different
-    values in the following order:
-    
-    - a REQUEST value, GET or POST, named "next" by default.
-    - LOGIN_REDIRECT_URL - the URL in the setting
-    - LOGIN_REDIRECT_URLNAME - the name of a URLconf entry in the settings
-    """
-    if login_redirect_urlname is None:
-        login_redirect_urlname = getattr(settings, "LOGIN_REDIRECT_URLNAME", "")
-    if login_redirect_urlname:
-        default_redirect_to = reverse(login_redirect_urlname)
-    else:
-        default_redirect_to = settings.LOGIN_REDIRECT_URL
-    redirect_to = request.REQUEST.get(redirect_field_name)
-    print redirect_to
-    if not redirect_to:
-        # try the session if available
-        if hasattr(request, "session"):
-            redirect_to = request.session.get(session_key_value)
-    # light security check -- make sure redirect_to isn't garabage.
-    if not redirect_to or "://" in redirect_to or " " in redirect_to:
-        redirect_to = default_redirect_to
-    return redirect_to
