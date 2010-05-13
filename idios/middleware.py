cb88d0fe6015e6c659c82bb3defe0035f9425ec6
Brian Rosner <brosner@gmail.com>
2010-05-13 03:20:40 -0600

    added additional info support for ensuring users have complete profiles

---
+++ idios/middleware.py
@@ -0,0 +1,70 @@
+import re
+
+from django import forms
+from django.conf import settings
+from django.db import models
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
+
+from idios.utils import get_profile_model
+
+
+class AdditionalInfoMiddleware(object):
+    
+    def process_request(self, request):
+        exemptions = [
+            r"^%s" % settings.MEDIA_URL,
+            r"^%s" % settings.STATIC_URL,
+            r"^/__debug__",
+        ]
+        for exemption in exemptions:
+            if re.match(exemption, request.path):
+                return None
+        kickstart = request.session.get("idios_additional_info_kickstart")
+        if kickstart:
+            return handle_additional_info(request)
+
+
+@csrf_protect
+def handle_additional_info(request):
+    if request.user.is_authenticated():
+        profile = request.user.get_profile()
+        missing_fields = []
+        # look for fields which are required on the model
+        for field in profile.idios_required_fields():
+            name = isinstance(field, tuple) and field[0] or field
+            db_field = profile._meta.get_field(name)
+            value = getattr(profile, db_field.attname)
+            if isinstance(db_field, (models.CharField, models.TextField)):
+                missing = not value
+            else:
+                missing = value is None
+            if missing:
+                if not isinstance(field, tuple):
+                    missing_fields.append((field, db_field.formfield()))
+                else:
+                    missing_fields.append(field)
+        if not missing_fields:
+            return None
+        attrs = {}
+        for field in missing_fields:
+            attrs[field[0]] = field[1]
+        AdditionalInfoForm = type("AdditionalInfoForm", (forms.Form,), attrs)
+        if request.method == "POST":
+            form = AdditionalInfoForm(request.POST, request.FILES)
+            if form.is_valid():
+                request.session.pop("idios_additional_info_kickstart", None)
+                for field, value in form.cleaned_data.iteritems():
+                    setattr(profile, field, value)
+                profile.save()
+                return None
+        else:
+            form = AdditionalInfoForm()
+        ctx = {
+            "form": form,
+        }
+        return render_to_response(
+            "idios/additional_info.html",
+            RequestContext(request, ctx)
+        )
--- idios/models.py
+++ idios/models.py
@@ -6,6 +6,10 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
 
+try:
+    from pinax.apps.account.signals import user_logged_in
+except ImportError:
+    user_logged_in = None
 
 
 class ProfileBase(models.Model):
@@ -23,3 +27,10 @@ class ProfileBase(models.Model):
     def get_absolute_url(self, group=None):
         # @@@ make group-aware
         return reverse("profile_detail", kwargs={"username": self.user.username})
+
+
+def additional_info_kickstart(sender, **kwargs):
+    request = kwargs.get("request")
+    request.session["idios_additional_info_kickstart"] = True
+if user_logged_in: # protect against Pinax not being available
+    user_logged_in.connect(additional_info_kickstart)
