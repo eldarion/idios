356b18035643b76967bea602b08066a6184aa7f8
Brian Rosner <brosner@gmail.com>
2010-11-16 13:48:48 -0700

    implemented lazy settings to allow import idios to work without Django and only call into Django when needed

--- idios/__init__.py
+++ idios/__init__.py
@@ -18,39 +18,52 @@ def get_version():
 __version__ = get_version()
 
 
-class IdiosSettings(object):
-    """
-    Combine the settings ``AUTH_PROFILE_MODULE`` and
-    ``IDIOS_PROFILE_MODULES`` into a single ``PROFILE_MODULES`` list.
-    
-    ``MULTIPLE_PROFILES`` is True if there is more than one profile model
-    defined, False otherwise.
-    
-    ``DEFAULT_PROFILE_MODULE`` is the profile model referenced by
-    ``AUTH_PROFILE_MODULE``, or the first one listed in
-    ``IDIOS_PROFILE_MODULES``.
-    
-    """
-    def __init__(self):
-        from django.conf import settings
-        
-        self.PROFILE_MODULES = []
-        
-        modules = []
-        mod = getattr(settings, "AUTH_PROFILE_MODULE", None)
-        if mod:
-            modules.append(mod)
-        
-        self.DEFAULT_PROFILE_MODULE = None
-        
-        for module in modules + getattr(settings, "IDIOS_PROFILE_MODULES", []):
-            if self.DEFAULT_PROFILE_MODULE is None:
-                self.DEFAULT_PROFILE_MODULE = module
-            self.PROFILE_MODULES.append(module)
-        
-        self.MULTIPLE_PROFILES = len(self.PROFILE_MODULES) > 1
-        
-        self.PROFILE_BASE = getattr(settings, "IDIOS_PROFILE_BASE", None)
+try:
+    import django
+except ImportError:
+    django = None
+else:
+    from django.utils.functional import LazyObject
 
-
-settings = IdiosSettings()
+if django:
+    class IdiosLazySettings(LazyObject):
+        
+        def _setup(self):
+            self._wrapped = IdiosSettings()
+    
+    
+    class IdiosSettings(object):
+        """
+        Combine the settings ``AUTH_PROFILE_MODULE`` and
+        ``IDIOS_PROFILE_MODULES`` into a single ``PROFILE_MODULES`` list.
+        
+        ``MULTIPLE_PROFILES`` is True if there is more than one profile model
+        defined, False otherwise.
+        
+        ``DEFAULT_PROFILE_MODULE`` is the profile model referenced by
+        ``AUTH_PROFILE_MODULE``, or the first one listed in
+        ``IDIOS_PROFILE_MODULES``.
+        
+        """
+        
+        def __init__(self):
+            from django.conf import settings
+            
+            self.PROFILE_MODULES = []
+            
+            modules = []
+            mod = getattr(settings, "AUTH_PROFILE_MODULE", None)
+            if mod:
+                modules.append(mod)
+            
+            self.DEFAULT_PROFILE_MODULE = None
+            
+            for module in modules + getattr(settings, "IDIOS_PROFILE_MODULES", []):
+                if self.DEFAULT_PROFILE_MODULE is None:
+                    self.DEFAULT_PROFILE_MODULE = module
+                self.PROFILE_MODULES.append(module)
+            
+            self.MULTIPLE_PROFILES = len(self.PROFILE_MODULES) > 1
+            self.PROFILE_BASE = getattr(settings, "IDIOS_PROFILE_BASE", None)
+    
+    settings = IdiosLazySettings()
