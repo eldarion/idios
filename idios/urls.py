384a02b3b2fdbd01b7ecc7645ec0dd5595348ca7
Michele Mattioni <mattions@gmail.com>
2012-06-14 16:48:26 +0100

    Fix the url order, to avoid the clash between usernames and the action url.
    With the previous order create and edit were recognised as username and a 404 page was given as response. This fix #9

--- idios/urls.py
+++ idios/urls.py
@@ -14,14 +14,15 @@ urlpatterns = patterns("idios.views",
     
     url(r"^$", ProfileListView.as_view(), name="profile_list"),
     url(r"^all/$", ProfileListView.as_view(all_profiles=True), name="profile_list_all"),
-    url(r"^(?P<profile_slug>[\w\._-]+)/$", ProfileListView.as_view(), name="profile_list"),
-    
-    profile_detail_default,
-    url(r"^(?P<profile_slug>[\w\._-]+)/profile/(?P<pk>\d+)/$", ProfileDetailView.as_view(), name="profile_detail"),
-    
+        
     url(r"^edit/$", ProfileUpdateView.as_view(), name="profile_edit"),
     url(r"^(?P<profile_slug>[\w\._-]+)/edit/$", ProfileUpdateView.as_view(), name="profile_edit"),
     
     url(r"^create/$", ProfileCreateView.as_view(), name="profile_create"),
     url(r"^(?P<profile_slug>[\w\._-]+)/create/$", ProfileCreateView.as_view(), name="profile_create"),
+    
+    profile_detail_default,
+    url(r"^(?P<profile_slug>[\w\._-]+)/profile/(?P<pk>\d+)/$", ProfileDetailView.as_view(), name="profile_detail"),
+    
+    url(r"^(?P<profile_slug>[\w\._-]+)/$", ProfileListView.as_view(), name="profile_list"),
 )
